699c242deb597e9750595e7a7841987c3893e250
Hardik Halani <hardik@codepiemedia.com>
2020-02-05 13:48:27 +0530

    Updates

---
+++ precache-manifest.7c4c179766e9ba9382a0ddd5d939c94d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31108cd49c3961807d9fd37ad14b0dd4",
+    "url": "/hardik98/Monster-mania/index.html"
+  },
+  {
+    "revision": "ff364a19ad9a8781aee8",
+    "url": "/hardik98/Monster-mania/static/css/main.4c57e57d.chunk.css"
+  },
+  {
+    "revision": "d8b503bbe8a07de5e2ac",
+    "url": "/hardik98/Monster-mania/static/js/2.aa5ce928.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/hardik98/Monster-mania/static/js/2.aa5ce928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff364a19ad9a8781aee8",
+    "url": "/hardik98/Monster-mania/static/js/main.1a917d00.chunk.js"
+  },
+  {
+    "revision": "0574236b67f5fff3f49c",
+    "url": "/hardik98/Monster-mania/static/js/runtime-main.a790467c.js"
+  }
+]);
